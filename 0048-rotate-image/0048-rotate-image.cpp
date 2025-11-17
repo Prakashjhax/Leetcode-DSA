ba62a24d688c32680ca219fbe43f6b93431fb838
Prakash Kumar Jha <jhaprakashkumar122@gmail.com>
2025-11-17 17:41:56 +0530

    0048-rotate-image

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
+    void rotate(vector<vector<int>>& matrix){
         int n = matrix.size();
-
-        // Step 1: Transpose the matrix
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                swap(matrix[i][j], matrix[j][i]);
-            }
-        }
-
-        // Step 2: Reverse each row
-        for (int i = 0; i < n; i++) {
-            reverse(matrix[i].begin(), matrix[i].end());
-        }
+        // Transpose krna hai
+        for(int i=0; i<n-1; i++)
+        {
+            for(int j=i+1; j<n; j++)
+            swap(matrix[i][j],matrix[j][i]);
+        } ;
+        // reverse each row 
+        for(int i=0; i<n; i++)
+        {
+            int start =0, end = n-1;
+            while(start<end)
+            {
+                swap(matrix[i][start],matrix[i][end]);
+                start++,end--;
+            } 
+        };
     }
 };
