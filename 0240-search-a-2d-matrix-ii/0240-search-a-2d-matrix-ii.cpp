cbd4839b81ae45783b3bf0909fadd678628c63e3
Prakash Kumar Jha <jhaprakashkumar122@gmail.com>
2025-11-19 23:06:23 +0530

    0240-search-a-2d-matrix-ii

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        int row = 0, col = n - 1; // start top-right
+        
+        while(row < m && col >= 0) {
+            int val = matrix[row][col];
+            
+            if(val == target) return true;
+            else if(val > target) col--;    // move left
+            else row++;                     // move down
+        }
+        
+        return false;
+    }
+};
