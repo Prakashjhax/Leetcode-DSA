a1f39d5af5ff956c1198e1149744e9b074c7a08c
Prakash Kumar Jha <jhaprakashkumar122@gmail.com>
2025-11-20 22:55:13 +0530

    0059-spiral-matrix-ii

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> mat(n, vector<int>(n));
+        
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        int num = 1;
+
+        while (top <= bottom && left <= right) {
+
+            // Left → Right
+            for (int i = left; i <= right; i++)
+                mat[top][i] = num++;
+            top++;
+
+            // Top → Bottom
+            for (int i = top; i <= bottom; i++)
+                mat[i][right] = num++;
+            right--;
+
+            // Right → Left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--)
+                    mat[bottom][i] = num++;
+                bottom--;
+            }
+
+            // Bottom → Top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--)
+                    mat[i][left] = num++;
+                left++;
+            }
+        }
+
+        return mat;
+    }
+};
