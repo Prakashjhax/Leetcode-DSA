d13cc5e799164c41c56097e838d80a3d38fa7c43
Prakash Kumar Jha <jhaprakashkumar122@gmail.com>
2025-12-23 22:09:08 +0530

    0071-simplify-path

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> st;
+        string curr = "";
+
+        for (int i = 0; i <= path.length(); i++) {
+            if (i == path.length() || path[i] == '/') {
+                if (curr == "" || curr == ".") {
+                    // ignore
+                }
+                else if (curr == "..") {
+                    if (!st.empty())
+                        st.pop_back();
+                }
+                else {
+                    st.push_back(curr);
+                }
+                curr = "";
+            } else {
+                curr += path[i];
+            }
+        }
+
+        string result = "/";
+        for (int i = 0; i < st.size(); i++) {
+            result += st[i];
+            if (i != st.size() - 1)
+                result += "/";
+        }
+
+        return result;
+    }
+};
